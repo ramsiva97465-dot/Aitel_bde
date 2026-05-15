868ab21ea64730d850e2a3d25469830e3e6f0614
Sivaram RS <ramsiva97465@gmail.com>
2026-05-15 16:30:25 +0530

    style: force search padding with inline style

--- src/pages/AllLeads.jsx
+++ src/pages/AllLeads.jsx
@@ -110,7 +110,8 @@ export default function AllLeads() {
             <Search size={15} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
             <input
               id="leads-search"
-              className="input-field pl-12"
+              className="input-field"
+              style={{ paddingLeft: '42px' }}
               placeholder="Search name, phone, company..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}

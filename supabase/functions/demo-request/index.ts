89b502585d314d1bbcfb3ceac2509aba6ef347f2
Sivaram RS <ramsiva97465@gmail.com>
2026-05-15 16:17:12 +0530

    chore: enable deno settings and fix ts errors in edge functions

--- supabase/functions/demo-request/index.ts
+++ supabase/functions/demo-request/index.ts
@@ -61,7 +61,7 @@ serve(async (req) => {
       { headers: { ...corsHeaders, 'Content-Type': 'application/json' }, status: 200 }
     )
 
-  } catch (error) {
+  } catch (error: any) {
     console.error('🔥 Server Error:', error.message)
     return new Response(
       JSON.stringify({ error: error.message }),
